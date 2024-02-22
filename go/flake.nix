0435216ae8118a77f4de96147a05947aa67f85e6
Matt Collinge <mattycov@googlemail.com>
2024-02-22 00:21:38 +0000

    added golang

---
+++ go/flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A nix-flake-based golang development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      allSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          packages = with pkgs;
+            [
+              go
+              gopls
+              golanfgci-lint
+              golangci-lint-langserver
+              gofumpt
+              go-tools
+              gotools
+              delve
+            ];
+        };
+      });
+    };
+}
+
